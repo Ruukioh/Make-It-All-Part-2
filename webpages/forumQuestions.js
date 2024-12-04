246b3d57b85e2adc4a36937add2e70abced68e80
Ruukioh <ruekanga4@gmail.com>
2024-12-04 15:26:08 +0000

    Add files via upload

---
+++ webpages/forumQuestions.js
@@ -0,0 +1,53 @@
+
+
+
+const forumQuestions = [
+    {
+        ID: 0,
+        user: "Sarah Jane Smith",
+        topic: "faq",
+        date: "26/10/2024",
+        question: "How do I use the printer, I don't know what button to press",
+        answers: [
+            {
+                user: "Alec Smith",
+                date: "27/10/2024",
+                response: "How can you not know how to use the printer?",
+            },
+            {
+                user: "Lola Lois",
+                date: "27/10/2024",
+                response: "load the paper into the printer and then press print.",
+            }
+        ]
+    },
+    {
+        ID: 1,
+        user: "John Johnson",
+        topic: "technical",
+        date: "25/10/2024",
+        question: "What to do if my anti-virus wont let me access a site?",
+        answers: [
+            {
+                user: "Robert Miller",
+                date: "25/10/2024",
+                response: "When this happened to me, I just used my own device to access the site, and copied the URL into an email to support and had them remove the block so I could use it again."
+            },
+            {
+                user: "Jennifer Davis",
+                date: "25/10/20024",
+                response: "Try a different site if possible, there's more than one source for information out there!",
+            },
+            {
+                user: "★: Susan Brown",
+                date: "09/87/5432",
+                response: "Robert and Jennifer are both partially correct. You can make a request for the site to be white-listed, or simply look for the information you need on another site, but I'd also like to suggest that you consider the reasoning given by the firewall as to why the site has been blacklisted/banned. If the firewall says that it's banned for being a 'malicious site' you should really be considering if the site is safe to be using on your own device at all."
+            }
+        ],
+        closed: "Answered"
+    }
+
+
+
+
+];
---
+++ webpages/sharedKnowledgeEntries.js
@@ -0,0 +1,52 @@
+
+
+
+const sharedKnowledgeEntries = [
+    {
+        id: 0,
+        title:"How to use the printer",
+        author:"asdf",
+        date:"16/10/2024",
+        preview: "<p>There are 2 ways to connect to the company's printers, to find out more click on Further Information<p>",
+        data:"<p>Printers are located at [printer locations]</p> <p>For a wired connection:</p> <ul> <li>Retrieve a usb cable from the storage cupboard </li><li>Insert one end of the cable into the back left side of the printer and the other into your laptop or PC</li><li>If the printer does not automatically connect and install the necessary drivers you should first try a different cable, then if that doesn't work call [insert tech help person]</li></ul> <p>For a wireless connection to a new printer on windows: </p> <ol> <li>Ensure the printer is first turned on </li><li>Click the start menu, then click on settings</li><li>Within settings click on Bluetooth & devices </li><li>Click on Printers & scanners </li><li>Click on Add device (next to Add a printer or scanner) </li><li>Wait for windows to detect all available printers </li></ol> <p> If the printer you're trying to connect to isn't found, check that your device is on the same WiFi network as the printer (e.g. not connected to a hotspot) </p>",
+        isModerated: false,
+        moderator: "",
+        tags:["non-technical","faq"]
+    },
+
+    {
+        id: 1,
+        title:"How to order printing paper",
+        author:"sdfg",
+        date:"16/10/2024",
+        preview: "<p>There are many ways to order printer paper, to find out more click on Further Information<p>",
+        data:"<p>Printer paper can be ordered in multiple ways</p><ul><li>Place your order online at one of these websites: www.thiswebsite.com www.thatwebsite.com</li><li>Enter the company's details: Contact : company email or phone number,Set delivery location to: company address,Set Payment details to: company credit card details</li><li>Review order making sure you've requested the right amount before submitting your order</li></ul><p>Alternatively you can:</p><ol><li>Collect a form from [office] and submit a request</li><li>Other method</li><li>Final method</li></ol><p>If you need further help, you can contact: [staff member] at [contact details]</p>",
+        isModerated:true,
+        moderator:"Sarah",
+        tags:["non-technical"]
+    },
+
+    {
+        id: 2,
+        title:"Operating system version for system computers",
+        author:"Billy",
+        date:"16/10/2024",
+        preview: "<p>The only operating system beng used is Windows XP, for a download link click on Further Information<p>",
+        data:"Windows XP",
+        isModerated:false,
+        moderator:"",
+        tags:["technical", "faq"]
+    },
+
+    {
+        id: 3,
+        title:"How to log onto admin system account",
+        author:"Lisa",
+        date:"17/10/2024",
+        preview: "Admin account details shouldn't be written down, for these details please click on Further Information",
+        data:"username : 'admin'<br>password : 'password123'",
+        isModerated:true,
+        moderator:"Sarah",
+        tags:["technical"]
+    },
+];
